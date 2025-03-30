f9f80874ed9fb5b19cbcb9e4ec6f51d8604e63b4
Arun <arun.@Aruns-MacBook-Pro-2.local>
2025-03-30 21:54:14 +0530

    Login

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 import express from "express";
-import fs from "fs";
 import bodyParser from "body-parser";
+import { PrismaClient } from "@prisma/client";
 
-const port=3000;
-const app=express();
+const port = 3000;
+const prisma = new PrismaClient();
+const app = express(); 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.send()
+app.get("/", (req, res) => {
+    res.render("Login.ejs");
+});
+
+app.post("/Home",async (req,res)=>{
+    const userdeails=await prisma.user.findUnique({
+        where : {email : req.body.givenemail , password : req.body.givenpass}
+    })
+    if (userdeails){
+        console.log("Find")
+        res.render("Home.ejs")
+    }else{
+        console.log("notfind")
+        res.redirect("/")
+    }
+    
 })
 
-app.listen(port,()=>{
-    console.log("running At 3000")
+app.get("/Reg",(req,res)=>{
+    res.render("Reg.ejs");
 })
+
+app.post("/submit", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        await prisma.user.create({
+            data: { name, email, password }
+        });
+        console.log("User Created:", { name, email });
+    } catch (error) {
+        console.error("Error:", error);
+    }
+    res.redirect("/");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
